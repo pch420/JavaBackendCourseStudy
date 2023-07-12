d1c0f9df9663888c78caf147bb94eb456f23afef
pch420 <pch420@naver.com>
2023-07-12 19:05:11 +0900

    java 백엔드 15회차 day11 2023.07.12 - java 프로젝트 생성 및 class 파일 처음 생성

---
+++ Study/BackStudy/Java8_Study/javaStudy/src/exam01/HelloTest.java
@@ -0,0 +1,31 @@
+package exam01;
+
+public class HelloTest {
+
+	public static void main(String[] args) {
+		// 주석문
+		// println 메서드는 값을 새로운 줄에 출력한다.
+		System.out.println("Hello World");
+		System.out.println("홍길동");
+		System.out.println();
+
+		// print 메서드는 값을 같은 줄에 출력한다.
+		System.out.print("Hello World");
+		System.out.print("홍길동");
+		System.out.println();
+		
+		// + 사용해서 여러 값을 연결해서 출력 가능 (값은 하나임)
+		System.out.println("홍길동"+20);
+		System.out.println();
+		
+		/*
+		 *  단축키
+		 *  1. 한줄 복사 : ctrl + alt + 화살표
+		 *  2. 줄 이동   : alt + 화살표
+		 *  3. 한 줄 삭제: ctrl + d
+		 *  4. 실행시킨 명령어 취소: ctrl + z
+		 *  5. sysout 입력후 ctrl + spacebar 
+		 */
+	}
+
+}
